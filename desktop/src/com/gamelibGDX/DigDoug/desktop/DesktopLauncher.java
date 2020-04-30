273d889e2054913bd6f5e7418833293c71d6961f
John Brosius <brosius01@gmail.com>
2020-04-30 16:16:25 -0500

    Update DesktopLauncher.java

--- desktop/src/com/gamelibGDX/DigDoug/desktop/DesktopLauncher.java
+++ desktop/src/com/gamelibGDX/DigDoug/desktop/DesktopLauncher.java
@@ -1,14 +1,27 @@
 package com.gamelibGDX.DigDoug.desktop;
 
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.gamelibGDX.DigDoug.DigDoug;
 
+import javax.swing.*;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 600;
 		config.height = 750;
-		new LwjglApplication(new DigDoug(), config);
+		int selection;
+		boolean isYes;
+		selection = JOptionPane.showConfirmDialog(null, "Would you like to play DigDug?");
+		isYes = (selection == JOptionPane.YES_OPTION);
+		if(isYes == true){
+			new LwjglApplication((ApplicationListener) new DigDoug(), config);
+		}
+		else if(!isYes){
+			JOptionPane.showConfirmDialog(null, "Coward");
+		}
 	}
 }
